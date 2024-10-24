d4cacf2c8e5fcb66d31755ee3391ceffa7c2a6a8
Emanuele Lena <emanuele.lena@designcoaching.net>
2024-10-24 15:36:32 +0200

    SST-975: fix issue in documentation

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -4,15 +4,16 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 # -- Path setup --------------------------------------------------------------
-import os
-import sys
-
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# sys.path.insert(0, os.path.abspath('../../src'))
+import os
+import sys
+
+
+sys.path.insert(0, os.path.abspath('../../src'))
 
 
 # -- Project information -----------------------------------------------------
