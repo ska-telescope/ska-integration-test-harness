3644a3e78c97f2d514e2facada08ab3d19a24371
Emanuele Lena <emanuele.lena@designcoaching.net>
2024-12-11 18:15:24 +0100

    SST-1012: add emulated MCCS tear down

--- src/ska_integration_test_harness/emulated/mccs_wrapper.py
+++ src/ska_integration_test_harness/emulated/mccs_wrapper.py
@@ -1,5 +1,8 @@
 """An emulated MCCS wrapper."""
 
+from ska_integration_test_harness.emulated.utils.teardown_helper import (
+    EmulatedTeardownHelper,
+)
 from ska_integration_test_harness.structure.mccs_wrapper import MCCSWrapper
 
 
@@ -13,31 +16,22 @@ class EmulatedMCCSWrapper(MCCSWrapper):
     # Specific MCCS methods and properties
 
     def tear_down(self) -> None:
-        """Tear down MCCS.
+        """Tear down the MCCS.
 
         The procedure is the following:
-
-        - TODO: Add
+        - Reset the health state for the MCCS master and the MCCS subarray.
+        - Clear the command call on the MCCS subarray.
+        - Reset the transitions data for the MCCS subarray.
+        - Reset the delay for the MCCS subarray.
         """
+        EmulatedTeardownHelper.reset_health_state(
+            [self.mccs_controller, self.mccs_subarray]
+        )
+        EmulatedTeardownHelper.clear_command_call([self.mccs_subarray])
+        EmulatedTeardownHelper.reset_transitions_data([self.mccs_subarray])
+        EmulatedTeardownHelper.reset_delay([self.mccs_subarray])
+        EmulatedTeardownHelper.unset_defective_status([self.mccs_subarray])
 
     def clear_command_call(self) -> None:
         """Clear the command call on the MCCS."""
-        # TODO: Add
-
-    # TODO: DO something similar to this
-    # def tear_down(self) -> None:
-    #     """Tear down the CSP.
-
-    #     The procedure is the following:
-    #     - Reset the health state for the CSP master and the CSP subarray.
-    #     - Clear the command call on the CSP subarray.
-    #     - Reset the transitions data for the CSP subarray.
-    #     - Reset the delay for the CSP subarray.
-    #     """
-    #     EmulatedTeardownHelper.reset_health_state(
-    #         [self.csp_master, self.csp_subarray]
-    #     )
-    #     EmulatedTeardownHelper.clear_command_call([self.csp_subarray])
-    #     EmulatedTeardownHelper.reset_transitions_data([self.csp_subarray])
-    #     EmulatedTeardownHelper.reset_delay([self.csp_subarray])
-    #     EmulatedTeardownHelper.unset_defective_status([self.csp_subarray])
+        EmulatedTeardownHelper.clear_command_call([self.mccs_subarray])
