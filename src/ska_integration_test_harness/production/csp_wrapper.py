54c34ec0071963bed4f6c02a6c0e2f8cbb97cf2b
Emanuele Lena <emanuele.lena@designcoaching.net>
2024-12-11 18:15:02 +0100

    SST-1012: add CSP  CBF serial number setting

--- src/ska_integration_test_harness/production/csp_wrapper.py
+++ src/ska_integration_test_harness/production/csp_wrapper.py
@@ -1,5 +1,6 @@
 """A wrapper for a production CSP."""
 
+import tango
 from assertpy import assert_that
 from ska_control_model import AdminMode, HealthState
 from ska_tango_testing.integration import TangoEventTracer
@@ -31,33 +32,22 @@ class ProductionCSPWrapper(CSPWrapper):
         super().__init__(csp_configuration)
         self.all_production = all_production
 
-        # When in Low, the PST device is needed and
-        # the admin mode must be set to ONLINE
         if self.config.supports_low():
+
+            # When in Low, the PST device is needed and
+            # the admin mode must be set to ONLINE
             self.ensure_admin_mode_online()
+
+            # When in Low, set the serial numbers in the CBF processor
+            self.set_serial_number_of_cbf_processor()
+
             # TODO: connection to PST crashed before, we have to
             # check if it continues to crash even after the synchronization
             # on the admin mode
             # self.pst = tango.DeviceProxy(self.config.pst_name)
 
-    # def set_serial_number_of_cbf_processor(self):
-    #     """Sets serial number for cbf processor"""
-    #     cbf_proc1 = DeviceProxy("low-cbf/processor/0.0.0")
-    #     cbf_proc2 = DeviceProxy("low-cbf/processor/0.0.1")
-
-    #     cbf_proc1.serialnumber = "XFL14SLO1LIF"
-    #     cbf_proc1.subscribetoallocator("low-cbf/allocator/0")
-    #     cbf_proc1.register()
-
-    #     cbf_proc2.serialnumber = "XFL1HOOQ1Y44"
-    #     cbf_proc2.subscribetoallocator("low-cbf/allocator/0")
-    #     cbf_proc2.register()
-
     def ensure_admin_mode_online(self) -> None:
         """Ensure the CSP master is in ONLINE admin mode."""
-
-        # TODO: this probably is needed for MCSS too
-
         if self.csp_master.adminMode != AdminMode.ONLINE:
             # set ADMIN mode to ONLINE
             self.csp_master.adminMode = AdminMode.ONLINE
@@ -89,6 +79,22 @@ class ProductionCSPWrapper(CSPWrapper):
         assert_that(self.csp_master.adminMode).described_as(
             "FAIL IN CSP SETUP: The CSP admin mode is supposed to be ONLINE."
         ).is_equal_to(AdminMode.ONLINE)
+
+    def set_serial_number_of_cbf_processor(self):
+        """Sets serial number for cbf processor.
+
+        TODO: is this something that should be parametrised?
+        """
+        cbf_proc1 = tango.DeviceProxy("low-cbf/processor/0.0.0")
+        cbf_proc2 = tango.DeviceProxy("low-cbf/processor/0.0.1")
+
+        cbf_proc1.serialnumber = "XFL14SLO1LIF"
+        cbf_proc1.subscribetoallocator("low-cbf/allocator/0")
+        cbf_proc1.register()
+
+        cbf_proc2.serialnumber = "XFL1HOOQ1Y44"
+        cbf_proc2.subscribetoallocator("low-cbf/allocator/0")
+        cbf_proc2.register()
 
     # --------------------------------------------------------------
     # Subsystem properties definition
