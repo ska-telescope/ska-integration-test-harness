907c28a4c3aaa8aeacd04c400df252280f5773bd
Emanuele Lena <emanuele.lena@designcoaching.net>
2025-01-02 10:09:09 +0100

    SST-1012: check MCCS is online

--- src/ska_integration_test_harness/production/mccs_wrapper.py
+++ src/ska_integration_test_harness/production/mccs_wrapper.py
@@ -53,15 +53,24 @@ class ProductionMCCSWrapper(MCCSWrapper):
         super().__init__(mccs_configuration)
 
         # set admin mode values of MCCS devices
-        self.set_admin_mode_values_mccs()
+        if self.mccs_controller.adminMode != AdminMode.ONLINE:
+            self.set_admin_mode_values_mccs()
 
     def set_admin_mode_values_mccs(self):
-        """Set the adminMode values of MCCS devices."""
+        """Set the adminMode values of MCCS devices.
+
+        (Procedure taken from here: https://gitlab.com/ska-telescope/mccs/ska-low-mccs/-/blob/main/notebooks/admin_mode_all_devices.ipynb?ref_type=heads)
+        """  # pylint: disable=line-too-long # noqa: E501
         db = tango.Database()
 
         for device_name in db.get_device_exported("low-mccs/*"):
             device = tango.DeviceProxy(device_name)
-            device.adminMode = AdminMode.ONLINE
+            if device.adminMode != AdminMode.ONLINE:
+                device.adminMode = AdminMode.ONLINE
+
+        # All devices should be online
+        for device_name in db.get_device_exported("low-mccs/*"):
+            assert tango.DeviceProxy(device_name).adminMode == AdminMode.ONLINE
 
     def is_emulated(self) -> bool:
         return False
