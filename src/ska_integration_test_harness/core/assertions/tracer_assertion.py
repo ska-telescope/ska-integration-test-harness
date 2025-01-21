95fa8c0754e09b6a93da19cd5a8ca9eef8fa51ef
Emanuele Lena <emanuele.lena@designcoaching.net>
2025-01-21 09:49:20 +0100

    SST-1018: improve tracer assertion documentation

--- src/ska_integration_test_harness/core/assertions/tracer_assertion.py
+++ src/ska_integration_test_harness/core/assertions/tracer_assertion.py
@@ -18,8 +18,7 @@ class TracerAssertion(SUTAssertion, abc.ABC):
     emitted by the TangoEventTracer. The assertion is event-based,
     so it needs to be configured with:
 
-    - a valid tracer instance (if omitted, a default one will be
-      created on-the-fly during setup);
+    - a valid tracer instance (if omitted, a default one will be used);
     - a timeout for the assertion to be verified (optional, if missing a
       zero timeout is assumed).
     - an early stop condition (optional, if missing the assertion will
@@ -42,13 +41,18 @@ class TracerAssertion(SUTAssertion, abc.ABC):
         """Create a new TracerAssertion instance.
 
         :param tracer: the TangoEventTracer instance to use for the assertion.
-            If omitted, an on-the-fly one will be created. Consider that if
-            you pass a tracer instance you have to take care about cleaning
-            it up.
+            If omitted, a default one will be used. **IMPORTANT**: if you
+            don't pass a tracer instance, the event and subscription
+            cleanup will be managed automatically during the setup phase,
+            but if you pass a tracer the cleanup will be left to you!
         :param timeout: the timeout for the assertion to be verified.
-            If omitted, a zero timeout is assumed.
+            If omitted, a zero timeout is assumed. You can pass a numeric
+            value to let this class create a timeout object for you, or
+            you can pass your own timeout object (maybe shared with other
+            assertions).
         :param early_stop: whether to stop the assertion early
-            in case of errors
+            in case of errors. If omitted, the assertion (if not passing)
+            will wait for the timeout to expire before failing.
         """
         self.tracer = tracer
         self.timeout: ChainedAssertionsTimeout = timeout
@@ -62,7 +66,6 @@ class TracerAssertion(SUTAssertion, abc.ABC):
         """
         return self._injected_tracer or self._on_the_fly_tracer
 
-    # setter for tracer property
     @tracer.setter
     def tracer(self, value: TangoEventTracer | None) -> None:
         """Set the tracer instance to use for the assertion.
@@ -74,6 +77,14 @@ class TracerAssertion(SUTAssertion, abc.ABC):
         self._on_the_fly_tracer = (
             None if value is not None else TangoEventTracer()
         )
+
+    def is_tracer_managed(self) -> bool:
+        """Check if the used tracer is managed by the assertion.
+
+        :return: True if the tracer is managed by the assertion, False
+            otherwise.
+        """
+        return self._injected_tracer is None
 
     @property
     def timeout(self) -> SupportsFloat:
@@ -89,7 +100,9 @@ class TracerAssertion(SUTAssertion, abc.ABC):
 
         This setter ensures the timeout is a chained timeout object
         (so eventual multiple calls of :py:meth:`get_assertpy_context` will
-        share the same timeout).
+        share the same timeout). If you pass a numeric value, a timeout
+        object will be created for you, otherwise you can pass your own
+        timeout object and maybe also share it with other assertions.
 
         :param value: the timeout for the assertion to be verified
         """
@@ -98,17 +111,23 @@ class TracerAssertion(SUTAssertion, abc.ABC):
     def setup(self) -> None:
         """Clean up an eventual managed tracer instance.
 
+        If the tracer is managed by the assertion, the cleanup will be
+        handled automatically during the setup phase.
+
         Please, override this and subscribe to the events you need to
         verify the assertion.
         """
-        if self._on_the_fly_tracer is not None:
+        if self.is_tracer_managed():
             self._on_the_fly_tracer.unsubscribe_all()
             self._on_the_fly_tracer.clear_events()
 
     def get_assertpy_context(self) -> Any:
         """Get the assertpy context to use for the assertion.
 
-        TODO: write usage example
+        Use this method to access a valid assertpy context that already
+        includes: the tracer instance, the timeout and the early stop
+        condition. Multiple calls of this method will share the same
+        timeout object.
 
         :return: the assertpy context to use for the assertion
         """
